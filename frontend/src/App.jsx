c7f08fd99eeafb30b66e9ba10b80d2c20a5cda16
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-26 15:03:50 +0530

    commit 7: redux toolkit and store setuped.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 import Home from "./pages/Home.jsx";
 import { useEffect } from "react";
 import { getCurrentUser } from "../features/getCurrentUser.js";
+import { useDispatch } from "react-redux";
+import { clearUserData, setUserData } from "./redux/userSlice.js";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const getUser = async () => {
+    try {
+      const data = await getCurrentUser();
+      dispatch(setUserData(data));
+    } catch (error) {
+      console.error("Sync current user failed:", error.message);
+      dispatch(clearUserData());
+    }
+  };
+
   useEffect(() => {
-    const getUser = async () => {
-      await getCurrentUser();
-    };
     getUser();
   }, []);
-  return <Home />;
+
+  return (
+    <>
+      <Home onLoginSuccess={getUser} />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,17 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../../utils/firebase.js";
 import api from "../../utils/axios.js";
 import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../redux/userSlice.js";
 
-function Home() {
+function Home({ onLoginSuccess }) {
+  const { userData } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  console.log(userData);
   const handleLogin = async (token) => {
     try {
       const { data } = await api.post("/api/auth/login", { token });
-      return data;
+      dispatch(setUserData(data));
     } catch (error) {
       const message =
         error.response?.data?.message || error.message || "Login failed";
@@ -21,6 +26,7 @@ function Home() {
       const data = await signInWithPopup(auth, googleProvider);
       const token = await data.user.getIdToken();
       await handleLogin(token);
+      await onLoginSuccess();
       console.log(data);
     } catch (error) {
       console.error(
@@ -32,24 +38,26 @@ function Home() {
   };
   return (
     <div className="min-h-screen bg-[#0d0f14] text-white overflow-hidden">
-      <div className="min-h-screen flex items-center justify-center px-4">
-        <div className="w-full max-w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
-          <div className="flex flex-col gap-1">
-            <h2 className="text-[17px] font-semibold text-slate-100 tracking-tight">
-              Welcome in SyntraAI
-            </h2>
-            <p className="text-[15px] text-slate-500">
-              Login or Signin to enjoy services.
-            </p>
+      {!userData && (
+        <div className="min-h-screen flex items-center justify-center px-4">
+          <div className="w-full max-w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
+            <div className="flex flex-col gap-1">
+              <h2 className="text-[17px] font-semibold text-slate-100 tracking-tight">
+                Welcome in SyntraAI
+              </h2>
+              <p className="text-[15px] text-slate-500">
+                Login or Signin to enjoy services.
+              </p>
+            </div>
+            <button
+              className="w-full flex items-center justify-center gap-3 py-2.75 rounded-xl text-sm font-medium text-white bg-linear-to-br from-indigo-500 to-violet-700 hover:from-indigo-400 hover:to-violet-600 active:from-indigo-600 active:to-violet-800 border border-indigo-500/30 shadow-lg shadow-indigo-500/20 hover:shadow-indigo-500/30 transition-all duration-150 cursor-pointer"
+              onClick={googleLogin}
+            >
+              <FcGoogle size={15} /> Continue with Google
+            </button>
           </div>
-          <button
-            className="w-full flex items-center justify-center gap-3 py-2.75 rounded-xl text-sm font-medium text-white bg-linear-to-br from-indigo-500 to-violet-700 hover:from-indigo-400 hover:to-violet-600 active:from-indigo-600 active:to-violet-800 border border-indigo-500/30 shadow-lg shadow-indigo-500/20 hover:shadow-indigo-500/30 transition-all duration-150 cursor-pointer"
-            onClick={googleLogin}
-          >
-            <FcGoogle size={15} /> Continue with Google
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
