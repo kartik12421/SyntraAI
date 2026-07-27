5c046c08977409caab9689e486ed573f25e92161
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-27 22:55:12 +0530

    commit 8: started making chat service. message and conversation schema is ready.

---
+++ backend/services/chat/models/message.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+    },
+    role: {
+      type: String,
+      enum: ["user", "assistant"],
+    },
+    content: String,
+  },
+  { timestamps: true },
+);
+
+const Message = mongoose.model("message", messageSchema);
+
+export default Message;
