3c26fcecbd101339fcedef8a5a6e647f9a734b42
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-30 17:22:54 +0530

    commit 12: started making the Sidebar component along with ChatArea component and Artifact component.

---
+++ frontend/src/components/ChatArea.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatArea() {
+  return (
+    <div className="flex flex-1 flex-col">
+      ChatArea
+    </div>
+  );
+}
+
+export default ChatArea;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import api from "../../utils/axios.js";
 import { FcGoogle } from "react-icons/fc";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserData } from "../redux/userSlice.js";
+import Sidebar from "../components/Sidebar.jsx";
+import ChatArea from "../components/ChatArea.jsx";
+import Artifact from "../components/Artifact.jsx";
 
 function Home({ onLoginSuccess }) {
   const { userData } = useSelector((state) => state.user);
@@ -39,6 +42,10 @@ function Home({ onLoginSuccess }) {
   };
   return (
     <div className="min-h-screen bg-[#0d0f14] text-white overflow-hidden">
+      <Sidebar />
+      <ChatArea />
+      <Artifact />
+
       {!userData && (
         <div className="min-h-screen flex items-center justify-center px-4">
           <div className="w-full max-w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
