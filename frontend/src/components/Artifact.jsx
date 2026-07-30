f480b93d8e019bad632e38d4fb365bfa680eb340
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-30 17:22:54 +0530

    commit 12: started making the Sidebar component along with ChatArea component and Artifact component.

---
+++ frontend/src/components/Artifact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Artifact() {
+  return (
+    <div className="hidden lg:flex h-full border border-white/6 flex-col overflow-hidden shrink-0 w-62.5">
+      Artifact
+    </div>
+  );
+}
+
+export default Artifact;
