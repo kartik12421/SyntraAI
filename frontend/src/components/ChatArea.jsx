70897dc3ea7d0e4e786c5178a6854f5b087b561d
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-08-01 22:25:01 +0530

    commit 15: Started making ChatArea.jsx.

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Nav from "./NAv";
+import MessageList from "./MessageList";
+import ChatInp from "./ChatInp";
 
 function ChatArea() {
   return (
     <div className="flex flex-1 flex-col">
-      ChatArea
+      <Nav />
+      <MessageList />
+      <ChatInp />
     </div>
   );
 }
