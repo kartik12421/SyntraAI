d881c761fdc451f4637bdd7100c312d0ced33f6b
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-08-03 22:09:28 +0530

    commit 17: adding functionality of fetching messages.

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "./Nav";
 import MessageList from "./MessageList";
 import ChatInp from "./ChatInp";
+import { useDispatch, useSelector } from "react-redux";
+import getMessages from "../../features/getMessages.js";
+import { setMessages } from "../redux/messageSlice.js";
 
 function ChatArea() {
+  const { selectedConversation } = useSelector((state) => state.conversation);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getMessage = async (params) => {
+      if (selectedConversation) {
+        const data = await getMessages(selectedConversation?._id);
+        dispatch(setMessages(data));
+      }
+    };
+
+    getMessage()
+  }, [selectedConversation]);
   return (
     <div className="flex min-w-0 flex-1 flex-col h-screen">
       <Nav />
