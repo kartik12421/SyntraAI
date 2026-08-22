c95126dfb31ddc0837f0719b4dfd48f5e280b605
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-08-22 19:28:11 +0530

    commit 50: vector stoere configured and embeddings are now able to store in Qdrant cloud.

--- backend/services/agent/agents/pdfRag.agent.js
+++ backend/services/agent/agents/pdfRag.agent.js
@@ -1,1 +1,30 @@
-export const pdfRag = async (state) => {};
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { vectorStore } from "../config/vectorDb.js";
+
+export const pdfRag = async (state) => {
+  try {
+    const buffer = fs.readFileSync(state.file.path);
+    const pdf = await PDFParse({
+      data: buffer,
+    });
+
+    const res = pdf.getText();
+    const text = res.text;
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 500,
+    });
+    const docs = splitter.splitText([text]);
+    const collectioNname = `pdf-${Date.now()}`;
+
+    const store = await vectorStore(docs, collectionName);
+  } catch (error) {
+    return {
+      ...state,
+      aiResponse: "Failed to analyze ppt 😓",
+    };
+  }
+};
